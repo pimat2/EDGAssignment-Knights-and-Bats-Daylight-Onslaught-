eae34dca3a5c47752ca8f51ced8b8625ce3cee09
DanielBatalski <ii145862@gmail.com>
2023-12-06 01:32:05 +0100

    Setting up the base for chunk spawning system

---
+++ Assets/Scripts/ChunkTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkTrigger : MonoBehaviour
+{
+	MapController mc;
+	
+	public GameObject targetMap;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    mc = FindObjectOfType<MapController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+	// Sent each frame where another object is within a trigger collider attached to this object (2D physics only).
+	private void OnTriggerStay2D(Collider2D col)
+	{
+		if(col.CompareTag("Player"))
+		{
+			mc.currentChunk = targetMap;
+		}
+	}
+	// Sent when another object leaves a trigger collider attached to this object (2D physics only).
+	private void OnTriggerExit2D(Collider2D col)
+	{
+		if(col.CompareTag("Player"))
+		{
+			if(mc.currentChunk == targetMap)
+			{
+				mc.currentChunk = null;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/MapController.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapController : MonoBehaviour
+{
+	public List<GameObject> terrainChunks;
+	public GameObject player;
+	public float checkerRadius;
+	Vector3 noTerrainPosition;
+	public LayerMask terraiMask;
+	public GameObject currentChunk;
+	PlayerMovement pm;
+	
+	[Header("Optimization")]
+	public List<GameObject> spawnedChunks;
+	public GameObject latestChunk;
+	public float maxOpsDistance; //must be higher and wider than the size of the tilemap
+	float opDistance;
+	float optimizerCooldown;
+	public float optimizerCooldownDuration;
+    // Start is called before the first frame update
+    void Start()
+    {
+	    pm = FindObjectOfType<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    ChunkChecker();
+	    ChunkOptimizer();
+	    
+    }
+    
+	void ChunkChecker()
+	{
+		if(!currentChunk)
+		{
+			return;
+		}
+		if(pm.moveDir.x > 0 && pm.moveDir.y == 0) //right
+		{
+			if(!Physics2D.OverlapCircle(currentChunk.transform.Find("Right").position, checkerRadius, terraiMask))
+			{
+				noTerrainPosition = player.transform.position + new Vector3(20,0,0);
+				SpawnChunk();	
+			}
+		}
+		else if(pm.moveDir.x < 0 && pm.moveDir.y == 0) //left
+		{
+			if(!Physics2D.OverlapCircle(currentChunk.transform.Find("Left").position, checkerRadius, terraiMask))
+			{
+				noTerrainPosition = player.transform.position + new Vector3(-20,0,0);
+				SpawnChunk();	
+			}
+		}
+		else if(pm.moveDir.x == 0 && pm.moveDir.y > 0) //up
+		{
+			if(!Physics2D.OverlapCircle(currentChunk.transform.Find("Top").position, checkerRadius, terraiMask))
+			{
+				noTerrainPosition = player.transform.position + new Vector3(0,20,0);
+				SpawnChunk();	
+			}
+		}
+		else if(pm.moveDir.x == 0 && pm.moveDir.y < 0) //down
+		{
+			if(!Physics2D.OverlapCircle(currentChunk.transform.Find("Bottom").position, checkerRadius, terraiMask))
+			{
+				noTerrainPosition = player.transform.position + new Vector3(0,-20,0);
+				SpawnChunk();	
+			}
+		}
+		else if(pm.moveDir.x > 0 && pm.moveDir.y > 0) //right up
+		{
+			if(!Physics2D.OverlapCircle(currentChunk.transform.Find("TopRight").position, checkerRadius, terraiMask))
+			{
+				noTerrainPosition = player.transform.position + new Vector3(20,20,0);
+				SpawnChunk();	
+			}
+		}
+		else if(pm.moveDir.x > 0 && pm.moveDir.y < 0) //rigt down
+		{
+			if(!Physics2D.OverlapCircle(currentChunk.transform.Find("BottomRight").position, checkerRadius, terraiMask))
+			{
+				noTerrainPosition = player.transform.position + new Vector3(20,-20,0);
+				SpawnChunk();	
+			}
+		}
+		else if(pm.moveDir.x < 0 && pm.moveDir.y > 0) //left up
+		{
+			if(!Physics2D.OverlapCircle(currentChunk.transform.Find("TopLeft").position, checkerRadius, terraiMask))
+			{
+				noTerrainPosition = player.transform.position + new Vector3(-20,20,0);
+				SpawnChunk();	
+			}
+		}
+		else if(pm.moveDir.x < 0 && pm.moveDir.y < 0) //left down
+		{
+			if(!Physics2D.OverlapCircle(currentChunk.transform.Find("BottomLeft").position, checkerRadius, terraiMask))
+			{
+				noTerrainPosition = player.transform.position + new Vector3(-20,-20,0);
+				SpawnChunk();	
+			}
+		}
+		
+	}
+	
+	void SpawnChunk()
+	{
+		int rand = Random.Range(0, terrainChunks.Count);
+		latestChunk = Instantiate(terrainChunks[rand], noTerrainPosition, Quaternion.identity);
+		spawnedChunks.Add(latestChunk);
+	}
+	
+	void ChunkOptimizer()
+	{
+		optimizerCooldown -= Time.deltaTime;
+		if(optimizerCooldown <= 0f)
+		{
+			optimizerCooldown = optimizerCooldownDuration;
+		}
+		else{
+			return;
+		}
+		foreach (GameObject chunk in spawnedChunks)
+		{
+			opDistance = Vector3.Distance(player.transform.position, chunk.transform.position);
+			if(opDistance > maxOpsDistance)
+			{
+				chunk.SetActive(false);
+			}
+			else
+			{
+				chunk.SetActive(true);	
+			}
+		}
+	}
+}
