1aa0c430fd245287573d94e89c66046bec8017cb
DanielBatalski <ii145862@gmail.com>
2023-12-13 23:45:17 +0100

    Adding logic for properly spawning enemies in waves
    Spawning of enemies is handled in waves, with each wave consisting of different groups of enemies. The spawning logic ensures that each group spawns the required number of enemies, and the overall wave quota is met. The spawning occurs at intervals defined by spawnInterval.

--- Assets/Scripts/EnemyScripts/EnemySpawner.cs
+++ Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -23,15 +23,60 @@ public class EnemySpawner : MonoBehaviour
 	}
 	
 	public List<Wave> waves; //A list of all the waves in the game
+	public int currentWaveCount; //The index of the current wave(starts from 0)
+	
+	[Header("Spawner Attributes")]
+	float spawnTimer;//timer for when to spawn the next enemy 
+	Transform player;
+	
     // Start is called before the first frame update
     void Start()
-    {
-        
+	{
+		player = FindObjectOfType<PlayerStats>().transform;
+		CalculateWaveQuota();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+	    spawnTimer += Time.deltaTime;
+	    //Checks if its time to spawn the next enemy
+	    if(spawnTimer >= waves[currentWaveCount].spawnInterval)
+	    {
+	    	spawnTimer = 0;
+	    	SpawnEnemies();
+	    }
     }
+	void CalculateWaveQuota()
+	{
+		int currentWaveQuota = 0;
+		foreach(var enemyGroup in waves[currentWaveCount].enemyGroups)
+		{
+			currentWaveQuota += enemyGroup.enemyCount;
+		}
+		
+		waves[currentWaveCount].waveQuota = currentWaveQuota;
+		Debug.Log(currentWaveQuota);
+	}
+	
+	void SpawnEnemies()
+	{
+		//Check if the minimum number of enemies in the wave have been spawned
+		if(waves[currentWaveCount].spawnCount < waves[currentWaveCount].waveQuota)
+		{
+			//Spawn each type of enemy until the quota is filled
+			foreach(var enemyGroup in waves[currentWaveCount].enemyGroups)
+			{
+				//Check if the minimum number of enemies of this type have been spawned
+				if(enemyGroup.spawnCount < enemyGroup.enemyCount)
+				{
+					Vector2 spawnPosition = new Vector2(player.transform.position.x + Random.Range(-10f, 10f), player.transform.position.y + Random.Range(-10f, 10f));
+					Instantiate(enemyGroup.enemyPrefab, spawnPosition, Quaternion.identity);
+					
+					enemyGroup.spawnCount++;
+					waves[currentWaveCount].spawnCount++;
+				}
+			}
+		}
+	}
 }
