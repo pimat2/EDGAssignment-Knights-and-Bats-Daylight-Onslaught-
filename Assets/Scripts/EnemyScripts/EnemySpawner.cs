e0438263a13281d046cdcefe131744e1adb8df87
DanielBatalski <ii145862@gmail.com>
2023-12-13 23:17:08 +0100

    Setting up the structure for an enemy spawner
    Enemy Spawner gets a nested class of EnemyGroup that defines the name, count and prefab type of enemy that will be spawned. Actual spawning of enemies will happen in second nested class named Wave that utilizes a list of enemy groups to spawn in wave.

---
+++ Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+	[System.Serializable]
+	public class Wave
+	{
+		public string waveName;
+		public List<EnemyGroup> enemyGroups;//A list of groups of enemies to spawn in this wave
+		public int waveQuota; //Total number of enemies to spawn in wave
+		public float spawnInterval; //Interval to spawn enemies
+		public float spawnCount; //Number of enemies already spawned in wave
+	}
+	[System.Serializable]
+	public class EnemyGroup
+	{
+		public string enemyName;
+		public int enemyCount; //Number of enemies to spawn in wave
+		public int spawnCount; //Number of enemies of this type already spawned in wave
+		public GameObject enemyPrefab;
+	}
+	
+	public List<Wave> waves; //A list of all the waves in the game
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
